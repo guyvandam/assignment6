02706a05bdbe40c694c8e90ce593bba732b09463
Guy Vandam <guyvandam@gmail.com>
2020-06-09 18:09:07 +0300

    finished part 1. almost done with the first level, need to show the level name.

--- src/gameobjects/gamelevels/DirectHitLevel.java
+++ src/gameobjects/gamelevels/DirectHitLevel.java
@@ -72,29 +72,25 @@ public class DirectHitLevel implements LevelInformation {
         return new Sprite() {
             @Override
             public void drawOn(DrawSurface d) {
-                int r = 50, halfALine = 3 * r + 20;
+                int r = 50, numOfCircles = 3, halfALine = numOfCircles * r + 20;
                 if (d != null) {
                     d.setColor(Color.BLACK);
                     d.fillRectangle(0, 0, getGuiWidth(), getGuiHeight());
-                    this.drawCircles(d, r);
+                    this.drawCircles(d, r,numOfCircles);
                     d.drawLine(centerX - halfALine, centerY, centerX + halfALine, centerY);
                     d.drawLine(centerX, centerY - halfALine, centerX, centerY + halfALine);
 
                 }
             }
 
-            public void drawCircles(DrawSurface d, int r) {
+            public void drawCircles(DrawSurface d, int r, int numOfCircles) {
                 if (d == null) {
                     return;
                 }
                 d.setColor(Color.BLUE);
-                int numOfCircles = 3;
                 for (int i = 0; i < numOfCircles; i++) {
-                    
+                    d.drawCircle(centerX, centerY, (i + 1) * r);
                 }
-                d.drawCircle(centerX, centerY, r);
-                d.drawCircle(centerX, centerY, 2 * r);
-                d.drawCircle(centerX, centerY, 3 * r);
             }
 
             @Override
@@ -107,7 +103,7 @@ public class DirectHitLevel implements LevelInformation {
     public List<Block> blocks() {
 //        int widthORHeight = 30;
 //        Block b = new Block(new Rectangle(new Point((this.getGuiWidth() - widthORHeight) / 2, (this.getGuiHeight() - widthORHeight) / 3), widthORHeight, widthORHeight), Color.RED);
-        Block b = new Block(new Rectangle(new Point((centerX - widthORHeight) / 2, (centerY - widthORHeight) / 2),
+        Block b = new Block(new Rectangle(new Point(centerX - (widthORHeight / 2), centerY - (widthORHeight / 2)),
                 widthORHeight, widthORHeight), Color.RED);
         return new ArrayList<>() {
             {
